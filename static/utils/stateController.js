b3f52566f6172d8d057ff759ffe41ff25bd0f99f
Nauany Cardoso <nauanycc@outlook.com.br>
2022-06-15 18:40:12 -0300

    refactor: destructure state properties from different places

--- static/utils/stateController.js
+++ static/utils/stateController.js
@@ -3,7 +3,8 @@ const { StateMachine } = require('./stateMachine.js');
 function generateControllersFromConfig({ entities }) {
   return Object.keys(entities).map((key) => {
     const instances = Object.keys(entities[key].instances).map((instanceId) => {
-      const { statesData, stateMachine } = entities[key].instances[instanceId];
+      const { statesData } = entities[key].instances[instanceId];
+      const { stateMachine } = entities[key];
 
       return {
         id: instanceId,
