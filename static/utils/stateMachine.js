128aafe687e1e245c808011ef8cb7509dfedd8cb
Nauany Cardoso <nauanycc@outlook.com.br>
2022-06-15 10:30:43 -0300

    feat: add state utils/class

---
+++ static/utils/stateMachine.js
@@ -0,0 +1,37 @@
+const { createMachine, interpret } = require('xstate');
+
+class StateMachine {
+  constructor(statesData, machine) {
+    this.statesData = statesData;
+    this.machine = createMachine(machine);
+    this.interpreter = interpret(this.machine).start();
+  }
+
+  getCurrentState() {
+    return this.interpreter.state.value;
+  }
+
+  setCurrentState(newState) {
+    this.interpreter.send(newState);
+  }
+
+  getStateData(state) {
+    return this.statesData[state];
+  }
+
+  getCurrentStateData() {
+    const currentState = this.getCurrentState();
+    return this.getStateData(currentState);
+  }
+}
+
+function getEntityInstance(controllers, entityType, instanceId) {
+  return controllers
+    .find((controller) => controller.entity === entityType)
+    .instances.find((instance) => instance.id === instanceId).stateMachine;
+}
+
+module.exports = {
+  StateMachine,
+  getEntityInstance,
+};
