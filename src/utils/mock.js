4c05515019f813ada3befec7a245f457a9265a71
Kevin Phillips <kphillips86@gmail.com>
2022-06-28 02:32:58 +0000

    updated mock generator to use graphql-tools/mock and handle nested fields

--- src/utils/mock.js
+++ src/utils/mock.js
@@ -1,4 +1,5 @@
-const { parse } = require("graphql");
+const { parse, buildSchema } = require("graphql");
+const { createMockStore } = require("@graphql-tools/mock");
 const casual = require("casual");
 
 const generateData = ({ name, type }) => {
@@ -19,49 +20,118 @@ const generateData = ({ name, type }) => {
   return name;
 };
 
-const getMockFieldsData = ({ typeFields, selectedFields, schema }) => {
-  const data = typeFields.reduce((mockFields, field) => {
-    if (field.kind === "FieldDefinition") {
-      const name = field.name.value;
+const getMocks = ({ ast }) => {
+  return ast.definitions.reduce((mocks, def) => {
+    const isTypeDef =
+      def.kind === "ObjectTypeDefinition" &&
+      def.name.value !== "Query" &&
+      def.name.value !== "Mutation";
 
-      if (!selectedFields || selectedFields[name]) {
-        if (field.type.kind === "NamedType") {
-          mockFields[name] = getMock({
-            schema,
-            entity: field.type.name.value,
-          });
-        } else {
-          const type = field.type.type.name.value;
-          mockFields[name] = generateData({ name, type });
+    if (isTypeDef) {
+      const mockFields = def.fields.reduce((mockFields, field) => {
+        const name = field.name.value;
+        const type = field.type.type?.name.value;
+
+        if (field.kind === "FieldDefinition" && type) {
+          mockFields[name] = () => generateData({ name, type });
         }
-      }
+
+        return mockFields;
+      }, {});
+
+      mocks[def.name.value] = mockFields;
     }
 
-    return mockFields;
+    return mocks;
   }, {});
-
-  return data;
 };
 
-const getMock = ({ schema, entity, fields }) => {
-  const ast = parse(schema);
+const merge = (a, b) => {
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+  const merged = {};
 
-  const typeFields = ast.definitions.find(
+  for (const key of aKeys) {
+    if (!bKeys.includes(key)) {
+      merged[key] = a[key];
+    } else {
+      if (typeof a[key] === "object") {
+        merged[key] = merge(a[key], b[key]);
+      }
+    }
+  }
+
+  for (const key of bKeys) {
+    if (!aKeys.includes(key)) {
+      merged[key] = b[key];
+    }
+  }
+
+  return merged;
+};
+
+// turns { keys: [ 'foo', 'bar'], value: 'baz' }
+// into { foo: { bar: 'baz' } }
+// and
+// turns { keys: [ 'foo', 'bar'], value: 'baz', data: { foo: { abc: 'xyz' } } }
+// into { foo: { abc: 'xyz', bar: 'baz' } }
+const generateNestedObjectWithValue = ({ keys, value, data }) => {
+  const reversed = keys.reverse();
+
+  const newData = reversed.slice(1).reduce(
+    (newData, key) => {
+      return {
+        [key]: newData,
+      };
+    },
+    { [reversed[0]]: value }
+  );
+
+  return merge(data, newData);
+};
+
+const getAllFieldsForEntity = ({ entity, ast }) => {
+  const fields = ast.definitions.find(
     (def) => def.name.value === entity
   ).fields;
 
-  // TODO - Cater for nested selection
-  const selectedFields =
-    fields &&
-    fields.reduce((selectFields, field) => {
-      selectFields[field] = 1;
+  return fields.reduce((fields, field) => {
+    const nestedFields =
+      field.type.kind === "NamedType"
+        ? getAllFieldsForEntity({ entity: field.type.name.value, ast }).map(
+            (nestedField) => `${field.name.value}.${nestedField}`
+          )
+        : [field.name.value];
 
-      return selectFields;
-    }, {});
+    return [...fields, ...nestedFields];
+  }, []);
+};
 
-  const mocks = getMockFieldsData({ typeFields, selectedFields, schema });
+const getMock = ({ schema, entity, fields }) => {
+  const key = Math.random();
 
-  return mocks;
+  const ast = parse(schema);
+  const store = createMockStore({
+    schema: buildSchema(schema),
+    mocks: getMocks({ ast }),
+  });
+
+  if (!fields) {
+    fields = getAllFieldsForEntity({ entity, ast });
+  }
+
+  return fields.reduce((data, field) => {
+    const splitField = field.split(".");
+
+    return {
+      ...data,
+      ...generateNestedObjectWithValue({
+        keys: splitField,
+        value: store.get(entity, key, splitField),
+        data,
+      }),
+    };
+  }, {});
 };
 
 module.exports = { getMock };
