92471266967c153e8aaf1f0e201d2521ba2f7661
Nauany Cardoso <nauanycc@outlook.com.br>
2022-07-26 11:52:35 -0300

    refactor: set entity instance interpreter id as it's own id

--- src/utils/state/stateController.ts
+++ src/utils/state/stateController.ts
@@ -1,11 +1,13 @@
-import { StateController } from "../../interfaces/state";
-import { StateMachine } from "./stateMachine";
+import { StateController } from '../../interfaces/state';
+import { StateMachine } from './stateMachine';
 
 export function getControllers(entities): Array<StateController> {
   return Object.keys(entities).map((key) => {
     const instances = Object.keys(entities[key].instances).map((instanceId) => {
       const { statesData } = entities[key].instances[instanceId];
       const { stateMachine } = entities[key];
+
+      stateMachine.id = instanceId;
 
       return {
         id: instanceId,
