5005f28ce4ee0b7e0f35f1e37f75408b6e174e24
George Ijidola <geordola@gmail.com>
2022-07-28 16:02:09 +0100

    Disable generator temporarily

--- bin/cli.js
+++ bin/cli.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 const { Command } = require("commander");
-const { gen, run } = require("./actions");
+const { run } = require("./actions");
 
 const program = new Command();
 
 const schemaOptions = ["-s, --schema <path>", "Path to GraphQL schema"];
-const entityOptions = [
-  "-e, --entity <name>",
-  "Entity to generate mock data for",
-];
-const fieldsOptions = [
-  "-f, --fields <fields>",
-  "Comma-separated list of fields to mock",
-];
+// const entityOptions = [
+//   '-e, --entity <name>',
+//   'Entity to generate mock data for',
+// ]
+// const fieldsOptions = [
+//   '-f, --fields <fields>',
+//   'Comma-separated list of fields to mock',
+// ]
 
 program
   .name("sms")
@@ -28,12 +28,12 @@ program
   .option("-p, --port <port>", "Port to run mock server")
   .action(run);
 
-program
-  .command("gen")
-  .description("CLI to generate mock data for an entity of a GraphQL schema")
-  .requiredOption(...schemaOptions)
-  .requiredOption(...entityOptions)
-  .option(...fieldsOptions)
-  .action(gen);
+// program
+//   .command("gen")
+//   .description("CLI to generate mock data for an entity of a GraphQL schema")
+//   .requiredOption(...schemaOptions)
+//   .requiredOption(...entityOptions)
+//   .option(...fieldsOptions)
+//   .action(gen);
 
 program.parse();
