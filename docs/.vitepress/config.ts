7093f2d2c54b04f9600fe80a875a0ff5fd9f8205
Marshall Thompson <marshall@creativeideal.net>
2022-08-23 18:16:28 -0600

    docs: add home page and placeholder getting-started page

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,22 @@
+export default {
+  themeConfig: {
+    siteTitle: 'Stateful Mock Server',
+    logo: '/stateful-mock-server-dark.svg',
+    footer: {
+      message: 'Built with ❤️ by Bitovi',
+    },
+    editLink: {
+      pattern: 'https://github.com/bitovi/stateful-mocks/edit/main/docs/:path',
+      text: 'Edit this page on GitHub',
+    },
+    sidebar: [
+      {
+        text: 'Guides',
+        items: [
+          { text: 'Getting Started', link: '/get-started' },
+          // { text: 'Understanding Mocks', link: '/getting-started' },
+        ],
+      },
+    ],
+  },
+}
