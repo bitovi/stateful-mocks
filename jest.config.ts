8e401b6185e6dc469094b8ee487c6d1c27c34f66
NauanyC <nauanycc@outlook.com.br>
2022-10-03 13:56:10 -0300

    refactor: move test folder outside src

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,5 @@
 export default {
-  roots: ["<rootDir>/src"],
+  roots: ["<rootDir>/tests"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
